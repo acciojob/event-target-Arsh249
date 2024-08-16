6359b150fe44cd51c845c3a9bc106d8653f5f09f
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-16 16:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map();
   }
-  
+
+  addEventListener(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
+    }
+    const listenersForEvent = this.listeners.get(event);
+    if (!listenersForEvent.includes(callback)) {
+      listenersForEvent.push(callback);
+    }
+  }
+
+  removeEventListener(event, callback) {
+    if (this.listeners.has(event)) {
+      const listenersForEvent = this.listeners.get(event);
+      const index = listenersForEvent.indexOf(callback);
+      if (index !== -1) {
+        listenersForEvent.splice(index, 1);
+      }
+      if (listenersForEvent.length === 0) {
+        this.listeners.delete(event);
+      }
+    }
+  }
+
+  dispatchEvent(event) {
+    if (this.listeners.has(event)) {
+      const listenersForEvent = this.listeners.get(event);
+      for (const listener of listenersForEvent) {
+        listener();
+      }
+    }
+  }
+}
+
+// Sample Usage
+const target = new EventTarget();
+
+const logHello = () => console.log('hello');
+const logWorld = () => console.log('world');
+
+target.addEventListener('hello', logHello);
+target.addEventListener('world', logWorld);
+
+target.dispatchEvent('hello'); // Console logs: hello
+target.dispatchEvent('world'); // Console logs: world
+
+target.removeEventListener('hello', logHello);
+
+target.dispatchEvent('hello'); // Does nothing
+target.dispatchEvent('world'); // Console logs: world
